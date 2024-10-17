c4e38452b5865d309a95b5cfb11d160069e578d7
Chayanan Reakmongkol (Champ) <105179966+Champr12@users.noreply.github.com>
2024-10-17 15:36:27 +0000

    Test cannot merge to main without passing checks

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "~> 1.9.5"
   backend "s3" {
-    bucket         = "gitops2024-tf-backend"
+    bucket        = "gitops2024-tf-backend"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "Gitops2024TerraformLocks"
