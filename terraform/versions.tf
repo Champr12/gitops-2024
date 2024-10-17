2bcb6b4443cb30a236efbe76948c47e8b93ae11d
Chayanan Reakmongkol (Champ) <105179966+Champr12@users.noreply.github.com>
2024-10-17 04:01:13 +0000

    Added S3 Backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,9 @@
-terraform {}
+terraform {
+  required_version = "~>= 1.9.5"
+  backend "s3" {
+    bucket = "gitops2024-tf-backend"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "Gitops2024TerraformLocks"
+  }
+}
